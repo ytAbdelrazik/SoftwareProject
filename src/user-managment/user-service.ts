fa0ead2a329932fc135aba41094b4d19c31b7250
ytAbdelrazik <yt_abdelrazik@outlook.com>
2024-11-23 20:00:18 +0100

    Update user-service.ts

--- src/user-managment/user-service.ts
+++ src/user-managment/user-service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { User } from 'src/user/users.schema';
-import { CreateUserdto } from './dtos/CreateUser.dto';
+import { User } from 'src/user-managment/users.schema';
+import { CreateUserdto } from './dots/CreateUser.dto';
 @Injectable()
 export class UserService {
   constructor(
