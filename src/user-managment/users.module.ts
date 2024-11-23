cf575b76eeaccd186086564e720e7bbb9760aab3
ytAbdelrazik <yt_abdelrazik@outlook.com>
2024-11-23 20:03:59 +0100

    Update users.module.ts

--- src/user-managment/users.module.ts
+++ src/user-managment/users.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UserService } from 'src/user/user-service';
-import { User, UserSchema } from 'src/user/users.schema';
+import { UserService } from 'src/user-managment/user-service';
+import { User, UserSchema } from 'src/user-managment/users.schema';
 import { UserController } from './user.controller';
 
 @Module({
