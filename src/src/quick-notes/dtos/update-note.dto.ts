1ee6f32351fec027adaff977b86979784cdbbe51
ahmedashraf06 <ashrafahmed2006@outlook.com>
2024-12-26 03:32:05 +0100

    Delete src/src directory

--- src/src/quick-notes/dtos/update-note.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsString, IsOptional } from 'class-validator';
-//validate datatypoe ONLY IF theres content/update.. optional
-export class UpdateNoteDto {
-  @IsOptional()
-  @IsString()
-  content?: string;
-}
-
--- src/src/user-managment/roles.decorator.ts
+++
@@ -1,7 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-
-// Define a constant key for roles metadata
-export const ROLES_KEY = 'roles';
-
-// Create a decorator to set roles metadata
-export const Roles = (...roles: string[]) => SetMetadata(ROLES_KEY, roles);
