f45e01d6552b4eb980b9df81250d75b587fd0211
MTamerElgendy <138935498+MTamerElgendy@users.noreply.github.com>
2024-12-26 23:03:05 +0100

    Update course-management.module.ts

--- src/course-management/course-management.module.ts
+++ src/course-management/course-management.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { CourseManagementController } from './course-management.controller';
 import { CourseManagementService } from './course-management.service';
+import { DiscussionsModule } from './discussions/discussions.module'; // Import DiscussionsModule
+import { CourseModule } from './course.module'; // Import CourseModule
 
 @Module({
+  imports: [
+    DiscussionsModule, // Register DiscussionsModule for discussion functionalities
+    CourseModule, // Register CourseModule for course functionalities
+  ],
   controllers: [CourseManagementController],
-  providers: [CourseManagementService]
+  providers: [CourseManagementService],
 })
 export class CourseManagementModule {}
