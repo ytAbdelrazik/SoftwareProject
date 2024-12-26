2870817e0e639179628367fcb2bf74d3322f7b10
MTamerElgendy <138935498+MTamerElgendy@users.noreply.github.com>
2024-12-26 23:32:06 +0100

    Update discussions.module.ts

--- src/course-management/discussions/discussions.module.ts
+++ src/course-management/discussions/discussions.module.ts
@@ -3,10 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { DiscussionsController } from './discussions.controller';
 import { DiscussionsService } from './discussions.service';
 import { Discussion, DiscussionSchema } from './discussions.schema';
+import { NotificationsModule } from '../../notifications/notifications.module'; // Correct import
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Discussion.name, schema: DiscussionSchema }]),
+    NotificationsModule,
+    
   ],
   controllers: [DiscussionsController],
   providers: [DiscussionsService],
