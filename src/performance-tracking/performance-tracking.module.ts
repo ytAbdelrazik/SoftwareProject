64d30c03922b57af0c408506c23bba57421d5633
hamza ehab rateb <116680691+hamzazaz@users.noreply.github.com>
2024-12-29 04:23:25 +0100

    Add files via upload

--- src/performance-tracking/performance-tracking.module.ts
+++ src/performance-tracking/performance-tracking.module.ts
@@ -9,23 +9,30 @@ import { ModuleSchema } from 'src/course-management/module.schema';
 import { ModuleModule } from 'src/course-management/module.module'; 
 import { Quiz, QuizSchema } from 'src/interactive-modules/quizzes.schema';
 import { Course, CourseSchema } from 'src/course-management/course.schema';
+import { Rating, RatingSchema } from './modulerating.schema';
+import { ResponseSchema } from 'src/interactive-modules/responses.schema';
+import { User, UserSchema } from 'src/user-managment/users.schema';
+import { InstructorRating, InstructorRatingSchema } from './instructorrating.schema';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-
   imports: [
     MongooseModule.forFeature([
       { name: Progress.name, schema: ProgressSchema },
       { name: UserInteraction.name, schema: UserInteractionSchema },
       { name: Module.name, schema: ModuleSchema }, 
       { name: Course.name, schema: CourseSchema },
-      { name: Quiz.name, schema: QuizSchema}
-      // Add this line
+      { name: Quiz.name, schema: QuizSchema },
+      { name: Rating.name, schema: RatingSchema },
+      { name: Response.name, schema: ResponseSchema },
+      { name: User.name, schema: UserSchema },
+      { name: InstructorRating.name, schema: InstructorRatingSchema }
     ]),
     CourseModule,
-    ModuleModule // Add CourseModule here
+    ModuleModule,
+    JwtModule.register({}), // Add this line
   ],
   controllers: [PerformanceTrackingController],
   providers: [PerformanceTrackingService],
 })
 export class PerformanceTrackingModule {}
-
